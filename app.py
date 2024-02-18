41d1bc4fdabf26d878002ff34493ddbb6df19ffb
RemxD9 <vladislav.iatsuk@urfu.me>
2024-02-18 21:14:30 +0500

    Практическая работа за 15.02.24

--- app.py
+++ app.py
@@ -1,43 +1,68 @@
 import datetime
+import random
+import re
+import os
 from flask import Flask
 
 app = Flask(__name__)
 
+cars_list = ['Chevrolet', 'Renault', 'Ford', 'Lada']
+cars_string = ', '.join(cars_list)
+cat_breeds = ['корниш-рекс', 'русская голубая', 'шотландская вислоухая', 'мейн-кун', 'манчкин']
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+BOOK_FILE = os.path.join(BASE_DIR, 'war_and_peace.txt')
+
+with open(BOOK_FILE, 'r', encoding='utf-8') as book:
+    book_content = book.read()
+
+words = [word for word in re.split(r'[.,;!?()\s\n\-]', book_content) if word]
+
 
 @app.route('/hello_world')
-def test_function():
-    pass
+def hello_world():
+    return 'Привет, мир!'
 
 
 @app.route('/cars')
-def test_function():
-    pass
+def cars():
+    return f'{cars_string}'
 
 
 @app.route('/cats')
-def test_function():
-    pass
+def cats():
+    random_breed = random.choice(cat_breeds)
+    return f'{random_breed}'
 
 
 @app.route('/get_time/now')
-def test_function():
-    pass
+def time_for_now():
+    current_time = datetime.datetime.now()
+    return f'Точное время: {current_time}'
 
 
 @app.route('/get_time/future')
-def test_function():
-    pass
+def future_time():
+    current_time = datetime.datetime.now()
+    one_hour = datetime.timedelta(hours=1)
+    current_time_after_hour = current_time + one_hour
+    return f'Точное время через час будет {current_time_after_hour}'
 
 
 @app.route('/get_random_word')
-def test_function():
-    pass
+def random_word():
+    word = random.choice(words)
+    return f'Случайное слово из романа Война и мир - {word}'
 
 
 @app.route('/counter')
-def test_function():
-    pass
+def counter():
+    counter.visits += 1
+    return f'Счетчик посещений страницы - {counter.visits}'
+
+
+counter.visits = 0
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port=5555, debug=True)
