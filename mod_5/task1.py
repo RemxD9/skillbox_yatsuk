1feefe8948d0642d21f5f80d67601cc2cee78cd3
RemxD9 <vladislav.iatsuk@urfu.me>
2024-03-11 00:47:57 +0500

    task 1 from mod 5

--- mod_5/task1.py
+++ mod_5/task1.py
@@ -1,46 +1,38 @@
-import os
 import subprocess
 import signal
-from flask import Flask, request
+import os
+from flask import Flask
 
 
 app = Flask(__name__)
 
 
-def port_is_in_use(port):
-    result = subprocess.run(['isof', '-i', f':{port}'], capture_output=True, text=True)
-    return True if result.stdout else False
+def is_port_in_use(port):
+    result = subprocess.run(['lsof', '-i', f':{port}'], capture_output=True, text=True)
+    return bool(result.stdout)
 
 
-def start_server():
-    port_number = 5000
-    app.run(debug=True, port=port_number)
-    if port_is_in_use(port_number):
-        terminating_process(port_number)
-    else:
-        app.run(debug=True, port=port_number)
-        terminating_process(port_number)
-    try:
-        app.run(debug=True, port=port_number)
-        print('Success!')
-        return
-    except Exception as e:
-        print(f'{e}')
-        return
-
-
-def terminating_process(port_number):
-    cmd = subprocess.run(['isof', '-i', f':{port_number}'], capture_output=True, text=True)
-    process_info = cmd.stdout.strip().split('\n')[1]
-    process_id = int(process_info.split()[1])
-    os.kill(process_id, signal.SIGTERM)
-    return
+def kill_process_using_port(port):
+    result = subprocess.run(['lsof', '-i', f':{port}'], capture_output=True, text=True)
+    if result.stdout:
+        process_info = result.stdout.strip().split('\n')[1]
+        process_id = int(process_info.split()[1])
+        os.kill(process_id, signal.SIGTERM)
+        print(f"Terminated process using port {port}")
 
 
 @app.route('/testing/')
-def testing_server():
-    print(f'Server started on {request.host}')
+def testing():
+    return 'Server has successfully started'
 
 
 if __name__ == '__main__':
-    start_server()
+    try:
+        app.run(debug=True)
+    except Exception as e:
+        if is_port_in_use(5000):
+            kill_process_using_port(5000)
+        try:
+            app.run(debug=True)
+        except Exception:
+            print(f'Failed:{e}')
